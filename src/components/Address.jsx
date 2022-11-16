87a78bdb82bc576b3b279ed61159924b6cba3277
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-11-16 18:00:38 +0900

    [fix] Address toggle

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -1,57 +1,66 @@
-import React, { useState, useEffect } from "react";
-import Button from "./styled-component/Button.jsx";
-import { SignupDes } from "./styled-component/SignupDes.jsx";
-import Postcode from "@actbase/react-daum-postcode";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import Button from './styled-component/Button.jsx';
+import { SignupDes } from './styled-component/SignupDes.jsx';
+import Postcode from '@actbase/react-daum-postcode';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Input from './styled-component/Input.jsx';
 
 export default function Address() {
-  const [address, setAddress] = useState("");
-  const [possible, setPossible] = useState(false);
-  const navigate = useNavigate();
+	const [address, setAddress] = useState('');
+	const [possible, setPossible] = useState(false);
+	const [openPostcode, setOpenPostcode] = useState(false);
+	const navigate = useNavigate();
+	const selectAddress = (data) => {
+		setAddress(data.address);
+		console.log(data.address);
+		setOpenPostcode(false);
+	};
+	const clickButton = () => {
+		setOpenPostcode((current) => !current);
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!address) {
+			return alert('주소를 입력해주세요.');
+		} else {
+			axios
+				.post('', {
+					address: address,
+				})
+				.then((res) => {
+					console.log(res.data);
+					if (res.data === 'success') {
+						alert('주소가 등록되었습니다.');
+						navigate('/complete');
+					}
+				})
+				.catch((err) => {
+					console.log(err);
+					navigate('/complete'); // 임시로 메인으로 가게 함
+				});
+		}
+	};
+	useEffect(() => {
+		if (address) {
+			setPossible(true);
+		} else if (!address) {
+			setPossible(false);
+		}
+	}, [address]);
 
-  const selectAddress = (data) => {
-    setAddress(data.address);
-    console.log(data.address);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!address) {
-      return alert("주소를 입력해주세요.");
-    } else {
-      axios
-        .post("", {
-          address: address,
-        })
-        .then((res) => {
-          console.log(res.data);
-          if (res.data === "success") {
-            alert("주소가 등록되었습니다.");
-            navigate("/complete");
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          navigate("/complete"); // 임시로 메인으로 가게 함
-        });
-    }
-  };
-
-  useEffect(() => {
-    if (address) {
-      setPossible(true);
-    } else if (!address) {
-      setPossible(false);
-    }
-  }, [address]);
-
-  return (
-    <>
-      <SignupDes>주소를 입력해주세요</SignupDes>
-      <Postcode onSelected={selectAddress} />
-      <div style={{ fontSize: "1.5rem", margin: "1.5rem 0" }}>{address}</div>
-      <Button onClick={handleSubmit} children={"완료"} possible={possible} />
-    </>
-  );
+	return (
+		<>
+			<SignupDes>주소를 입력해주세요</SignupDes>
+			{openPostcode && (
+				<Postcode
+					autoClose={false}
+					onSelected={selectAddress}
+					style={{ width: '100%', height: '200px' }}
+				/>
+			)}
+			<Input id="addressInput" onClick={clickButton} value={address} />
+			<Button onClick={handleSubmit} children={'완료'} possible={possible} />
+		</>
+	);
 }
