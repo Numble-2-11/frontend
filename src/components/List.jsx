82f6f60bc0ff141bfe0496952dd64bd20c11d6c8
parkyejin <uiop5809@naver.com>
2022-11-18 18:24:07 +0900

    [design] 마이페이지 카테고리 구현

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,17 +5,17 @@ const StyledList = styled.li`
   padding: 0.5rem 0 1rem 0;
   list-style: none;
   font-size: 1.2rem;
-  width: 8rem;
-  font-weight: 600;
+  width: ${(props) => (props.width ? props.width : "8rem")} 
+  font-weight: bold;
   border-bottom: ${(props) =>
     props.isSelected ? "0.2rem solid #2AD4AF" : "0.2rem solid #fff"};
   opacity: ${(props) => (props.isSelected ? "1" : "0.5")};
   color: ${(props) => (props.isSelected ? "#2AD4AF" : "white")};
 `;
 
-export default function List({ isSelected, children, id }) {
+export default function List({ isSelected, children, id, style }) {
   return (
-    <StyledList isSelected={isSelected} id={id}>
+    <StyledList isSelected={isSelected} id={id} style={style}>
       {children}
     </StyledList>
   );
--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,8 +1,60 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Header from "../components/Header.jsx";
+import List from "../components/List.jsx";
+import styled from "styled-components";
+import { Outlet } from "react-router-dom";
+import OutSpan from "./../components/mypage/OutSpan";
+import ProfileImg, { Img } from "../components/styled-component/ProfileImg";
 
 export default function Mypage() {
   const { id } = useParams();
+  const [selected, setSelected] = useState(1);
 
-  return <div>Mypage {id}</div>;
+  const list = [
+    { text: "프로필", url: "profile", id: "1" },
+    { text: "친구 목록", url: "friendList", id: "2" },
+    { text: "내가 쓴 글", url: "myWriting", id: "3" },
+  ];
+
+  const handleClick = (e) => {
+    if (e.target.outerText === "프로필") {
+      setSelected(1);
+    } else if (e.target.outerText === "친구 목록") {
+      setSelected(2);
+    } else if (e.target.outerText === "내가 쓴 글") {
+      setSelected(3);
+    }
+  };
+
+  return (
+    <>
+      <Header children={"마이페이지"} />
+      <ProfileImg />
+      <Container>
+        {list.map((item) => (
+          <Link
+            to={item.url}
+            key={item.id}
+            onClick={handleClick}
+            style={{ display: "flex", width: "100%" }}
+          >
+            <List isSelected={selected == item.id} style={{ flex: 1 }}>
+              {item.text}
+            </List>
+          </Link>
+        ))}
+      </Container>
+      <Outlet />
+      <div style={{ display: "flex" }}>
+        <OutSpan style={{ marginRight: "1rem" }}>탈퇴하기</OutSpan>
+        <OutSpan>로그아웃</OutSpan>
+      </div>
+    </>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  margin-bottom: 2.6rem;
+`;
