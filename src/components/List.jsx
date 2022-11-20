91a89e275fb73286baeedb98bb5f4a9564c6b5e0
parkyejin <uiop5809@naver.com>
2022-11-21 00:28:53 +0900

    [design] 메뉴바 border-bottom 수정

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,7 +8,9 @@ const StyledList = styled.li`
   width: ${(props) => (props.width ? props.width : "8rem")};
   font-weight: ${(props) => (props.isSelected ? "bold" : "300")};
   border-bottom: ${(props) =>
-    props.isSelected ? "0.2rem solid #2AD4AF" : "0.2rem solid #fff"};
+    props.isSelected
+      ? "0.3rem solid #2AD4AF"
+      : "0.2rem solid rgba(255, 255, 255, 0.3)"};
   opacity: ${(props) => (props.isSelected ? "1" : "0.3")};
   color: ${(props) => (props.isSelected ? "#2AD4AF" : "white")};
 `;
