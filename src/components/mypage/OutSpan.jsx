3fc8e54d9c51348b08fa3930ac5c1681ec3dec2e
parkyejin <uiop5809@naver.com>
2022-11-21 15:34:08 +0900

    [feat] 마이페이지 Dialog

--- src/components/mypage/OutSpan.jsx
+++ src/components/mypage/OutSpan.jsx
@@ -5,8 +5,13 @@ const StyleSpan = styled.span`
   font-size: 1.1rem;
   font-weight: 400;
   opacity: 0.7;
+  cursor: pointer;
 `;
 
-export default function OutSpan({ children, style }) {
-  return <StyleSpan style={style}>{children}</StyleSpan>;
+export default function OutSpan({ children, style, onClick }) {
+  return (
+    <StyleSpan style={style} onClick={onClick}>
+      {children}
+    </StyleSpan>
+  );
 }
