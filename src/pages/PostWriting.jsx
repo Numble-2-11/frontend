0af88436afdb515db68e9dfded8528048c969d45
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-11-23 13:40:35 +0900

    [feat] 게시글 작성 페이지 기능 추가

--- src/pages/PostWriting.jsx
+++ src/pages/PostWriting.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import GrayButton from '../components/styled-component/GrayButton';
+import Input from '../components/styled-component/Input';
 import PostInput from '../components/styled-component/PostInput';
 import TextArea from '../components/styled-component/TextArea';
+import imgIcon from '../images/imgIcon.svg';
 
 export default function PostWriting() {
+	function onClickUpload() {
+		let imageInput = document.getElementById('image-input');
+		imageInput.click();
+	}
 	return (
 		<>
-			<Header children={'게시글 작성'} />
-			<GrayButton
-				children={'저장'}
-				style={{
-					padding: '0.9rem 1.4rem',
-					width: '5.1rem',
-					height: '3.2rem',
-					fontSize: '1.2rem',
-					borderRadius: '1.6rem',
-				}}
-			/>
+			<Header>
+				게시글 작성
+				<GrayButton children={'저장'} margin={'0 0 0 4rem'} />
+			</Header>
 			<PostInput type="text" name="title" placeholder="제목" isTitle={true} />
 			<TextArea
 				id="contents"
 				name="contents"
 				placeholder="내용을 입력해주세요"
-				rows="5"
+				rows="8"
 				cols="33"
 			></TextArea>
 			<PostInput
@@ -34,6 +33,34 @@ export default function PostWriting() {
 			/>
 			<br></br>
 			<PostInput type="text" name="dong" placeholder="행정동" isTitle={false} />
+			<div style={{ margin: '4rem 0 0 0 ', textAlign: 'left' }}>
+				<GrayButton children={'새로생겼어요'} />
+				<GrayButton children={'행사/축제'} />
+				<GrayButton children={'세일/이벤트'} />
+				<GrayButton children={'소모임'} />
+				<GrayButton children={'잃어버렸어요'} />
+				<GrayButton children={'자유게시판'} />
+			</div>
+			<label for="image-input">
+				<GrayButton
+					style={{
+						width: '100%',
+						marginTop: '2rem',
+						height: '4.2rem',
+						borderRadius: '2rem',
+					}}
+					dozen={true}
+					onClick={onClickUpload}
+				>
+					<img src={imgIcon} /> <span>이미지를 추가해보세요</span>
+				</GrayButton>
+			</label>
+			<Input
+				id="image-input"
+				type="file"
+				style={{ display: 'none' }}
+				accept="image/*"
+			/>
 		</>
 	);
 }
