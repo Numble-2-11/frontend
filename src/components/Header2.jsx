91ff5a4b0e71f45189fd122d19f16ae93185aa49
parkyejin <uiop5809@naver.com>
2022-11-28 17:30:44 +0900

    [design] 게시글 작성 Header

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -3,15 +3,17 @@ import { useNavigate } from "react-router-dom";
 import searchIcon from "../images/search.svg";
 import arrow_left from "../images/arrow_left.svg";
 import Title from "./styled-component/Title";
+import GrayButton from "./posting/GrayButton.jsx";
 
 const Head = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   margin: 1.5rem 0 2.5rem 0;
+  flex: 1;
 `;
 
-export default function Header2({ children, search, marginBottom }) {
+export default function Header2({ children, search, marginBottom, store }) {
   const navigate = useNavigate();
 
   return (
@@ -19,7 +21,12 @@ export default function Header2({ children, search, marginBottom }) {
       <img
         src={arrow_left}
         onClick={() => navigate(-1)}
-        style={{ width: "2.2rem", height: "1.8rem", cursor: "pointer" }}
+        style={{
+          width: "2.2rem",
+          marginRight: store ? "2.1rem" : "0",
+          height: "1.8rem",
+          cursor: "pointer",
+        }}
       />
       <Title children={children} />
       {search ? (
@@ -28,7 +35,13 @@ export default function Header2({ children, search, marginBottom }) {
           style={{ width: "2.2rem", height: "2.2rem", cursor: "pointer" }}
         />
       ) : (
+        ""
+      )}
+      {store ? <GrayButton children={"저장"} margin={"0"} /> : ""}
+      {!search && !store ? (
         <div style={{ width: "2.2rem", height: "2.2rem" }} />
+      ) : (
+        ""
       )}
     </Head>
   );
--- src/pages/PostWriting.jsx
+++ src/pages/PostWriting.jsx
@@ -1,66 +1,63 @@
-import React, { useState } from 'react';
-import Header from '../components/Header';
-import GrayButton from '../components/styled-component/GrayButton';
-import Input from '../components/styled-component/Input';
-import PostInput from '../components/styled-component/PostInput';
-import TextArea from '../components/styled-component/TextArea';
-import imgIcon from '../images/imgIcon.svg';
+import React from "react";
+import Header2 from "../components/Header2";
+import GrayButton from "../components/posting/GrayButton";
+import Input from "../components/styled-component/Input";
+import PostInput from "../components/posting/PostInput";
+import TextArea from "../components/posting/TextArea";
+import imgIcon from "../images/imgIcon.svg";
 
 export default function PostWriting() {
-	function onClickUpload() {
-		let imageInput = document.getElementById('image-input');
-		imageInput.click();
-	}
-	return (
-		<>
-			<Header>
-				게시글 작성
-				<GrayButton children={'저장'} margin={'0 0 0 4rem'} />
-			</Header>
-			<PostInput type="text" name="title" placeholder="제목" isTitle={true} />
-			<TextArea
-				id="contents"
-				name="contents"
-				placeholder="내용을 입력해주세요"
-				rows="8"
-				cols="33"
-			></TextArea>
-			<PostInput
-				type="text"
-				name="storeName"
-				placeholder="가게 이름"
-				isTitle={false}
-			/>
-			<br></br>
-			<PostInput type="text" name="dong" placeholder="행정동" isTitle={false} />
-			<div style={{ margin: '4rem 0 0 0 ', textAlign: 'left' }}>
-				<GrayButton children={'새로생겼어요'} />
-				<GrayButton children={'행사/축제'} />
-				<GrayButton children={'세일/이벤트'} />
-				<GrayButton children={'소모임'} />
-				<GrayButton children={'잃어버렸어요'} />
-				<GrayButton children={'자유게시판'} />
-			</div>
-			<label for="image-input">
-				<GrayButton
-					style={{
-						width: '100%',
-						marginTop: '2rem',
-						height: '4.2rem',
-						borderRadius: '2rem',
-					}}
-					dozen={true}
-					onClick={onClickUpload}
-				>
-					<img src={imgIcon} /> <span>이미지를 추가해보세요</span>
-				</GrayButton>
-			</label>
-			<Input
-				id="image-input"
-				type="file"
-				style={{ display: 'none' }}
-				accept="image/*"
-			/>
-		</>
-	);
+  function onClickUpload() {
+    let imageInput = document.getElementById("image-input");
+    imageInput.click();
+  }
+  return (
+    <>
+      <Header2 store={true}>게시글 작성</Header2>
+      <PostInput type="text" name="title" placeholder="제목" isTitle={true} />
+      <TextArea
+        id="contents"
+        name="contents"
+        placeholder="내용을 입력해주세요"
+        rows="8"
+        cols="33"
+      ></TextArea>
+      <PostInput
+        type="text"
+        name="storeName"
+        placeholder="가게 이름"
+        isTitle={false}
+      />
+      <br></br>
+      <PostInput type="text" name="dong" placeholder="행정동" isTitle={false} />
+      <div style={{ margin: "4rem 0 0 0 ", textAlign: "left" }}>
+        <GrayButton children={"새로생겼어요"} />
+        <GrayButton children={"행사/축제"} />
+        <GrayButton children={"세일/이벤트"} />
+        <GrayButton children={"소모임"} />
+        <GrayButton children={"잃어버렸어요"} />
+        <GrayButton children={"자유게시판"} />
+      </div>
+      <label for="image-input">
+        <GrayButton
+          style={{
+            width: "100%",
+            marginTop: "2rem",
+            height: "4.2rem",
+            borderRadius: "2rem",
+          }}
+          dozen={true}
+          onClick={onClickUpload}
+        >
+          <img src={imgIcon} /> <span>이미지를 추가해보세요</span>
+        </GrayButton>
+      </label>
+      <Input
+        id="image-input"
+        type="file"
+        style={{ display: "none" }}
+        accept="image/*"
+      />
+    </>
+  );
 }
